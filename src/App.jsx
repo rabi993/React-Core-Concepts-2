992c7c10f76363947e820c2619741fdbd5c13195
rabi993 <rabi.21.pust@gmail.com>
2025-04-25 11:19:46 +0600

    React-Core-Concepts-2 started with hook

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+import Counter from './counter';
+import Team from './team';
+
+function App() {
+  function handleClick(){
+    alert('button Clicked');
+  }
+  const handleClick2 = () => {
+    alert('button Clicked 2');
+  }
+  const addToFive = (num)=>{
+    alert(num + 5);
+  }
+
+
+
+  return (
+    <>
+      
+      <h3>React Core Concepts 2</h3>
+      <Team></Team>
+      <Counter></Counter>
+      {/* <button onclick="handleClick()">Clic me</button> */}
+      <button onClick={handleClick}>Clic me</button>
+      <button onClick={handleClick2}>Clic me2</button>
+      <button onClick={()=>{alert('button Clicked 3')}}>Clic me3</button>
+      {/* <button onClick={addToFive(3)}>Clic me4</button> */}
+      <button onClick={()=> addToFive(3)}>Clic me4</button>
+    </>
+  )
+}
+
+export default App
+
+
